611ebb69134a3c2ef3632806c901eceb4eb7b1b9
Enrique Marenco <wilmer_marenco@hotmail.com>
2022-11-22 21:22:40 -0600

    Agregar archivos de proyecto.

---
+++ semana16-1/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace semana16_1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome()
+        {
+            return "Esta es la acción de Bienvenida";
+        }
+
+        public string Parameters(string name, int edad)
+        {
+            return HtmlEncoder.Default.Encode($"Hola {name}, tu edad es: {edad} años");
+        }
+    }
+}
---
+++ semana16-1/Data/semana16-1Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using semana16_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace semana16_1.Data
+{
+    public class semana16_1Context : DbContext
+    {
+        public semana16_1Context(DbContextOptions<semana16_1Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+
+    }
+}
